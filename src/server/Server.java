7f3bd90f2634c6aa1e9d4b7e504cc2317cee0306
macforsong <981579206@qq.com>
2020-11-03 12:10:11 +0800

    网络实验3

---
+++ src/server/Server.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package server;
+import interface_.MeetingInterface;
+import interface_.UserInterface;
+
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+/**
+ * 远程服务器
+ * @author ZhangHan
+ *
+ */
+public class Server {
+
+	public static void main(String[] args) throws RemoteException,
+	MalformedURLException {
+
+	/**
+	 * Step 1:创建远程对象
+	 */
+	MeetingInterface meetingInterface = new MeetingImplement();
+	UserInterface userInterface = new UserImplement();
+	/**
+	 * Step 2:注册端口号
+	 */
+	LocateRegistry.createRegistry(1099);
+	/**
+	 * Step 3:绑定远程对象
+	 */
+	Naming.rebind("MeetingRemote", meetingInterface);
+	Naming.rebind("UserRemote", userInterface);
+	/**
+	 * Step 4:服务器启动完毕
+	 */
+	System.out.println("服务器启动成功");
+	}
+}
