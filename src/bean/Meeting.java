27e56fab80d6dc815493c1d7012a8820df1112ff
macforsong <981579206@qq.com>
2020-11-06 00:23:16 +0800

    提交版1

--- src/bean/Meeting.java
+++ src/bean/Meeting.java
@@ -1,106 +1,104 @@
 /**
- *
+ * 
  */
 package bean;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 /**
  * MEET CLASS
  * @author macforsong
  *
  */
-public class Meeting implements Serializable {
+public class Meeting implements Serializable{
 
-    private int meetingId;//会议的ID
-    private Date startTime;//开始时间
-    private Date endTime;//结束时间
-    private String title;//标题
-    private ArrayList<User> participants = new ArrayList<User>();//参与者
+	private int meetingId;//会议的ID
+	private Date startTime;//开始时间
+	private Date endTime;//结束时间
+	private String title;//标题
+	private ArrayList<User> participants=new ArrayList<User>();//参与者
 
-    /**
-     * 无参构造函数
-     */
-    public Meeting() {
-    }
+	/**
+	 * 无参构造函数
+	 */
+	public Meeting(){
+	}
 
-    /**
-     * 有参构造函数
-     * @param meetingId 会议的ID
-     * @param startTime 开始时间
-     * @param endTime 结束时间
-     * @param title 会议的标题
-     * @param participants 参与者
-     */
-    public Meeting(int meetingId, Date startTime, Date endTime, String title,
-                   ArrayList<User> participants) {
-        super();
-        this.meetingId = meetingId;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.title = title;
-        this.participants = participants;
-    }
-
-
-    public int getMeetingId() {
-        return meetingId;
-    }
+	/**
+	 * 有参构造函数
+	 * @param meetingId 会议的ID
+	 * @param startTime 开始时间
+	 * @param endTime 结束时间
+	 * @param title 会议的标题
+	 * @param participants 参与者
+	 */
+	public Meeting(int meetingId, Date startTime, Date endTime, String title,
+			ArrayList<User> participants) {
+		super();
+		this.meetingId = meetingId;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.title = title;
+		this.participants = participants;
+	}
 
 
-    public void setMeetingId(int meetingId) {
-        this.meetingId = meetingId;
-    }
+	public int getMeetingId() {
+		return meetingId;
+	}
 
 
-    public Date getStartTime() {
-        return startTime;
-    }
+	public void setMeetingId(int meetingId) {
+		this.meetingId = meetingId;
+	}
 
 
-    public void setStartTime(Date startTime) {
-        this.startTime = startTime;
-    }
+	public Date getStartTime() {
+		return startTime;
+	}
 
 
-    public Date getEndTime() {
-        return endTime;
-    }
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
 
 
-    public void setEndTime(Date endTime) {
-        this.endTime = endTime;
-    }
+	public Date getEndTime() {
+		return endTime;
+	}
 
 
-    public String getTitle() {
-        return title;
-    }
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
 
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
 
-    public ArrayList<User> getParticipants() {
-        return participants;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
 
-    public void setParticipants(ArrayList<User> participants) {
-        this.participants = participants;
-    }
+	public ArrayList<User> getParticipants() {
+		return participants;
+	}
 
 
-    @Override
-    public String toString() {
-        return "Meeting" + meetingId + " [startTime=" + startTime
-                + ", endTime=" + endTime + ", title=" + title
-                + ", participants=" + participants + "]";
-    }
+	public void setParticipants(ArrayList<User> participants) {
+		this.participants = participants;
+	}
 
+
+	@Override
+	public String toString() {
+		return "Meeting"+meetingId+" [startTime=" + startTime
+				+ ", endTime=" + endTime + ", title=" + title
+				+ ", participants=" + participants +"]";
+	}
+	
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,8 +1,7 @@
 /**
- *
+ * 
  */
 package server;
-
 import rface.MeetingInterface;
 import rface.UserInterface;
 
@@ -10,7 +9,6 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
-
 /**
  * 远程服务器
  * @author macforsong
@@ -18,26 +16,26 @@ import java.rmi.registry.LocateRegistry;
  */
 public class Server {
 
-    public static void main(String[] args) throws RemoteException,
-            MalformedURLException {
+	public static void main(String[] args) throws RemoteException,
+	MalformedURLException {
 
-        /**
-         * 创建远程对象
-         */
-        MeetingInterface meetingInterface = new MeetingImplement();
-        UserInterface userInterface = new UserImplement();
-        /**
-         * 注册端口号
-         */
-        LocateRegistry.createRegistry(1099);
-        /**
-         * 绑定远程对象
-         */
-        Naming.rebind("MeetingRemote", meetingInterface);
-        Naming.rebind("UserRemote", userInterface);
-        /**
-         * 服务器启动完毕
-         */
-        System.out.println("======服务器启动成功======");
-    }
+	/**
+	 * Step 1:创建远程对象
+	 */
+	MeetingInterface meetingInterface = new MeetingImplement();
+	UserInterface userInterface = new UserImplement();
+	/**
+	 * Step 2:注册端口号
+	 */
+	LocateRegistry.createRegistry(1099);
+	/**
+	 * Step 3:绑定远程对象
+	 */
+	Naming.rebind("MeetingRemote", meetingInterface);
+	Naming.rebind("UserRemote", userInterface);
+	/**
+	 * Step 4:服务器启动完毕
+	 */
+	System.out.println("服务器启动成功");
+	}
 }
